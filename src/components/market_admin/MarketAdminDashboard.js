40444fdfff255f74e3212fb8d24840678185208c
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-06-14 11:49:46 +0100

    MarketAdminDashboard converted

--- src/components/market_admin/MarketAdminDashboard.js
+++ src/components/market_admin/MarketAdminDashboard.js
@@ -1,30 +1,29 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { MarketAdminAuthorization } from '../../utils';
-import { useQuery } from 'react-apollo-hooks';
+import { Query } from "react-apollo";
 import { MARKET_ADMIN_DATA_QUERY } from '../../graphql-types';
 import AdminMarketTile from './AdminMarketTile';
 import CreateMarketForm from './CreateMarketForm'
 import '../../styling/MarketTile.css';
 
 function MarketAdminDashboard(props) {
-  const { data, error, loading } = useQuery(MARKET_ADMIN_DATA_QUERY, {});
   const [newMarketForm, setNewMarketForm] = useState(false);
   
   if (!MarketAdminAuthorization()) return <Redirect to='/market_admin/login' />;
 
-  if (loading) return <div>Loading...</div>;
-  
-  if (error) return <div>Error! {error.message}</div>;
-
-  // const { name, blurb, address, geoLocation, directions, imgUrl, openHours, traders } = data.administeredMarkets;
-  // const { id, email, firstName, lastName } = data.marketAdminData;
-  
-  const renderMarkets = () => {
-    return data.administeredMarkets.map(market => 
-      <AdminMarketTile key={market.id} market={market} />
-    )
-  };
+  const renderMarkets = () => (
+    <Query query={MARKET_ADMIN_DATA_QUERY} >
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error: {error.message}</p>;
+        
+        return data.administeredMarkets.map(market => 
+          <AdminMarketTile key={market.id} market={market} />
+        );
+      }}
+    </Query>
+  );
 
   if(newMarketForm) return (
     <div className='market-admin-dashboard dashboard'>
