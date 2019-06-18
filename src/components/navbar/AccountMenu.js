a007c9885f0f1f24f1901d33fe6b48e9cee82b63
Cully-Curwen <c.t.g.curwen@gmail.com>
2019-06-18 16:03:30 +0100

    Navbar stylied draft 1, and svg in

--- src/components/navbar/AccountMenu.js
+++ src/components/navbar/AccountMenu.js
@@ -1,14 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, Route } from 'react-router-dom';
+import { CustomerAuthorization, MarketAdminAuthorization, TraderAdminAuthorization } from '../../utils';
+import './styling/AccountMenu.css';
+import basket from '../../icons/shopping-basket-solid.svg';
+import menuBars from '../../icons/bars-solid.svg';
 
 function AccountMenu(props) {
+  const [menuClick, setMenuClick] = useState(false);
+  
+  if (CustomerAuthorization() | MarketAdminAuthorization() | TraderAdminAuthorization()) {
+    return (<></>);
+  }; 
+
   return (
-    <div className="account-menu">
-      <Route path='/market/:id' component={props => (
-        <NavLink to={'/market/'+ props.match.params.id +'/basket'} ><button >Market Basket</button></NavLink>
-      )} />
-      <NavLink to='/customer/shopping_carts' ><button >Shopping Carts</button></NavLink>
-      <NavLink exact to='/' ><button onClick={() => localStorage.clear()} >Log Out</button></NavLink>
+    <div className="w3-bar">
+      {CustomerAuthorization() &&
+        <Route path='/market/:id' component={props => (
+          <NavLink 
+            to={'/market/'+ props.match.params.id +'/basket'} 
+            className="w3-bar-item w3-button"
+          >
+            <img src={basket} alt='Market Basket' className="basket" />
+          </NavLink>
+        )} />
+      }
+      <div className="w3-dropdown-click">
+        <div className="w3-bar-item w3-button" onClick={() => setMenuClick(!menuClick)} >
+          <img src={menuBars} alt='Account-menu' className="menu-bars" />
+        </div>
+        <div 
+          className={'dropdown w3-dropdown-content w3-bar-block' + (menuClick && ' w3-show') } 
+          onClick={() => setMenuClick(!menuClick)}
+        >
+          {CustomerAuthorization() &&
+            <NavLink 
+              to='/customer/shopping_carts'
+              className="w3-bar-item w3-button" 
+            >Shopping Carts</NavLink>
+          }
+          <NavLink 
+            exact 
+            to='/' 
+            className="w3-bar-item w3-button" 
+          ><div onClick={() => {localStorage.clear();}} >Log Out</div></NavLink>
+        </div>
+      </div>
+      <div className="w3-clear"></div>
     </div>
   );
 };
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,44 +1,29 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import '../../styling/NavBar.css';
+// import '../../styling/NavBar.css';
 import { Route } from 'react-router-dom';
-import { CustomerAuthorization, MarketAdminAuthorization, TraderAdminAuthorization } from '../../utils';
 import AccountMenu from "./AccountMenu";
 import MarketName from './MarketName'
 
 function NavBar(props) {
-  
-  const authorized = () => {
-    if (CustomerAuthorization()) return (
-      <AccountMenu />
-    );
-    if (MarketAdminAuthorization()) return (
-      <AccountMenu />
-    ); 
-    if (TraderAdminAuthorization()) return (
-      <AccountMenu />
-    ); 
-    return ( <h3>Welcome</h3> ); 
-  };
 
   return (
-    <div className='navbar' >
-      <div className="nav-div list">
+    <div className='w3-bar w3-green' >
+      <div className="w3-bar-item w3-left list">
         <NavLink 
           exact
           to='/customer/markets'
-          activeStyle={{
-            fontWeight: "bold",
-            color: "red"
-          }}
-        ><h4>Market List</h4></NavLink>
+          className="w3-bar-item w3-button"
+          >
+          Market List
+        </NavLink>
       </div>
-      <div className="nav-div market">
+      <div className="w3-bar-item w3-center market">
         <Route path='/market/:id' render={props => <MarketName {...props} pathSeg={'market'} />} />
         <Route path='/trader/:id' render={props => <MarketName {...props} pathSeg={'trader'} />} />
       </div>
-      <div className="nav-div account">
-        {authorized()}
+      <div className="w3-bar-item w3-right account">
+        <AccountMenu />
       </div>
     </div>
   );
